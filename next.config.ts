85bb368fa931346105e4984d14343a08c280793c
Koihjygh Hdhsh <koihjyghhdhsh@gmail.com>
2025-12-21 15:36:42 +0000

    Update config CommitFix

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,20 @@
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
     
-      // 1. अगर टाइपिंग की गलती हो, तो भी बिल्ड न रोकें
-        typescript: {
-            ignoreBuildErrors: true,
-              },
-                
-                  // 2. अगर कोड में सफाई (Linting) की कमी हो, तो भी न रोकें
-                    eslint: {
-                        ignoreDuringBuilds: true,
-                          },
+      eslint: {
+          ignoreDuringBuilds: true,
+            },
+              
+                typescript: {
+                    ignoreBuildErrors: true,
+                      },
 
-                            // 3. इमेज एरर रोकने के लिए
-                              images: {
-                                  unoptimized: true,
-                                    },
-                                    };
+                        images: {
+                            unoptimized: true,
+                              },
+                              };
 
-                                    export default nextConfig;
-                                    
+                              export default nextConfig;
+                              
