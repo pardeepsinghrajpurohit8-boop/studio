7c6b38c849ba6e6f50c04820ee362a77360dc5fd
Koihjygh Hdhsh <koihjyghhdhsh@gmail.com>
2025-12-21 17:49:28 +0000

    Add manifest

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,25 @@
+// @ts-nocheck
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output: "export",
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-};
+const withPWA = require("next-pwa")({
+  dest: "public",
+    register: true,
+      skipWaiting: true,
+        disable: process.env.NODE_ENV === "development",
+        });
 
-export default nextConfig;
+        const nextConfig: NextConfig = {
+          output: "export",
+            eslint: {
+                ignoreDuringBuilds: true,
+                  },
+                    typescript: {
+                        ignoreBuildErrors: true,
+                          },
+                            images: {
+                                unoptimized: true,
+                                  },
+                                  };
+
+                                  export default withPWA(nextConfig);
+                                  
